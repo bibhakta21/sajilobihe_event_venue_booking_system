e3c3fb3f9bfa531698eb757552daae71dd61c55e
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-12-20 12:23:51 +0545

    Added bookmark and order view screen

---
+++ lib/view/Bottom_Screen/order_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrderView extends StatelessWidget {
+  const OrderView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Order page',
+        style: TextStyle(fontSize: 24, fontFamily: 'Montserrat Italic'),
+      ),
+    );
+  }
+}
