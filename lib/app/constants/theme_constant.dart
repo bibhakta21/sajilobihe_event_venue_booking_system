4d67d26c20e7b79f9e3957b1e26bbf2cb1718d5f
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-01-17 18:23:34 +0545

    added custom UI components and theme constants

---
+++ lib/app/constants/theme_constant.dart
@@ -0,0 +1,7 @@
+// app/constants/theme_constant.dart
+import 'package:flutter/material.dart';
+
+class ThemeConstant {
+  static const Color secondaryColor = Colors.white;
+  static const Color primaryColor = Colors.redAccent;
+}
---
+++ lib/core/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    primarySwatch: Colors.red,
+    scaffoldBackgroundColor: Colors.white,
+    fontFamily: 'Montserrat',
+  );
+}
+
