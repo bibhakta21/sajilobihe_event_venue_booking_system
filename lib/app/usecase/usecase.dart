d9ec66406432ebbd4b55daec1626384dfec5aa5c
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-01-17 18:10:58 +0545

    define generic use case interfaces for business logic

---
+++ lib/app/usecase/usecase.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:sajilobihe_event_venue_booking_system/core/error/failure.dart';
+
+abstract interface class UsecaseWithParams<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
+
+abstract interface class UsecaseWithoutParams<SuccessType> {
+  Future<Either<Failure, SuccessType>> call();
+}
