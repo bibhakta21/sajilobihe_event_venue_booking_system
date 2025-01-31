8df35d1ffabc7ee619135b65cd224e66e485f23c
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-01-31 20:27:55 +0545

    Define failure handling classes for API, local database, and shared preferences

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -2,21 +2,34 @@ import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
   final String message;
-
-  const Failure({required this.message});
+  final int? statusCode;
+  const Failure({required this.message, this.statusCode});
 
   @override
   List<Object> get props => [message];
+
+  @override
+  String toString() => 'Failure(message: $message, statusCode: $statusCode)';
 }
 
 class LocalDatabaseFailure extends Failure {
-  const LocalDatabaseFailure({required super.message});
+  const LocalDatabaseFailure({
+    required super.message,
+  });
 }
 
 class ApiFailure extends Failure {
+  @override
   final int statusCode;
-  const ApiFailure(
-    this.statusCode, {
+
+  const ApiFailure({
+    required this.statusCode,
+    required super.message,
+  });
+}
+
+class SharedPrefsFailure extends Failure {
+  const SharedPrefsFailure({
     required super.message,
   });
 }
