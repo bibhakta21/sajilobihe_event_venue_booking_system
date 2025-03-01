1bd272c92177765e6db40c3e7a1737640b3fadfb
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-03-01 17:11:51 +0545

    deleted all unnecessary code

--- lib/features/auth/data/data_source/auth_data_source.dart
+++
@@ -1,9 +0,0 @@
-import 'package:sajilobihe_event_venue_booking_system/features/auth/domain/entity/auth_entity.dart';
-
-abstract interface class IAuthDataSource {
-  Future<String> loginUser(String email, String password);
-
-  Future<void> registerUser(AuthEntity user);
-
-  Future<AuthEntity> getCurrentUser();
-}
